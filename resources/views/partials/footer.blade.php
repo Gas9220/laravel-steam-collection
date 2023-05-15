051f3d6ec569b5deb635af31cae91418d561a92a
vittoriaromano <vittoria5005@gmail.com>
2023-05-15 17:54:38 +0200

    creata struttura

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,1 @@
+<footer>ciaox</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="container d-flex content-center">
+        <div class="logo">
+            <img src="#" alt="logo">
+            <h1>Nome bello</h1>
+        </div>
+    </div>
+</header>
