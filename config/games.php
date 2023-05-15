ad72023c0cb5b49224480de0edd908fbc120731b
daniloeberli <danilo.eberli@gmail.com>
2023-05-15 18:10:18 +0200

    fix: added description

--- config/games.php
+++ config/games.php
@@ -11,8 +11,8 @@ return [
         "genre" => 'sandbox, survival',
         "rating" => 9.8,
         "thumbnail" => 'https://www.codiciprodotto.it/wp-content/uploads/2021/01/minecraft.jpg',
-        "early_access" => 0
-        
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Grand Theft Auto V",
@@ -24,7 +24,8 @@ return [
         "genre" => 'Action-adventure',
         "rating" => 9.9,
         "thumbnail" => 'https://www.gtabase.com/igallery/8501-8600/gta-v-cover-ps5-xbox-series-xs-8584-1920.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Red Dead Redemption 2",
@@ -36,7 +37,8 @@ return [
         "genre" => 'Action-adventure',
         "rating" => 9.7,
         "thumbnail" => 'https://www.gtabase.com/igallery/4201-4300/RDR_2_Cover_PC-4213-1920.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "The Elder Scrolls V: Skyrim",
@@ -48,7 +50,8 @@ return [
         "genre" => 'Action role-playing',
         "rating" => 9.3,
         "thumbnail" => 'https://upload.wikimedia.org/wikipedia/en/1/15/The_Elder_Scrolls_V_Skyrim_cover.png',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Fortnite",
@@ -60,7 +63,8 @@ return [
         "genre" => 'Battle royale',
         "rating" => 8.4,
         "thumbnail" => 'https://gaming-cdn.com/images/products/2500/orig-fallback-v1/fortnite-pc-gioco-epic-games-cover.jpg?v=1645021449',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "World of Warcraft",
@@ -72,7 +76,8 @@ return [
         "genre" => 'Massively multiplayer online role-playing',
         "rating" => 7.9,
         "thumbnail" => 'https://m.media-amazon.com/images/I/510Xjw4h8kS._AC_SY445_.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "The Last of Us",
@@ -84,7 +89,8 @@ return [
         "genre" => '',
         "rating" => 9.4,
         "thumbnail" => 'https://static.posters.cz/image/750/poster/the-last-of-us-key-art-i127761.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Super Mario 64",
@@ -96,7 +102,8 @@ return [
         "genre" => '',
         "rating" => 8.0,
         "thumbnail" => 'https://upload.wikimedia.org/wikipedia/it/thumb/3/3e/Super_Mario_64.jpg/640px-Super_Mario_64.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Batman: Arkham City",
@@ -108,7 +115,8 @@ return [
         "genre" => 'Action-adventure',
         "rating" => 8.1,
         "thumbnail" => 'https://static.posters.cz/image/750/poster/batman-arkham-city-cover-i11510.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Half-Life 2",
@@ -120,7 +128,8 @@ return [
         "genre" => 'First-person shooter',
         "rating" => 8.1,
         "thumbnail" => 'https://www.gamerclick.it/immagini/videogame/Half_Life_2/cover/Half_Life_2-cover.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Mass Effect 2",
@@ -132,7 +141,8 @@ return [
         "genre" => 'Action role-playing, third-person shooter',
         "rating" => 8.1,
         "thumbnail" => 'https://static.wikia.nocookie.net/masseffectitalia/images/4/4b/ME2_Soundtrack_Cover.png/revision/latest?cb=20141022104223&path-prefix=it',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Dark Souls",
@@ -144,7 +154,8 @@ return [
         "genre" => 'Action role-playing',
         "rating" => 8.1,
         "thumbnail" => 'https://upload.wikimedia.org/wikipedia/en/8/8d/Dark_Souls_Cover_Art.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Silent Hill 2",
@@ -156,7 +167,8 @@ return [
         "genre" => 'Survival horror',
         "rating" => 8.1,
         "thumbnail" => 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1057256e-383f-4417-a343-5eae9d369263/ddxi6lp-75cd791d-aa99-416f-806e-96b121b6270b.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzEwNTcyNTZlLTM4M2YtNDQxNy1hMzQzLTVlYWU5ZDM2OTI2M1wvZGR4aTZscC03NWNkNzkxZC1hYTk5LTQxNmYtODA2ZS05NmIxMjFiNjI3MGIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.HuMGkht9C1nEkPghhz65U-Qj6MZcBPQB1Ug0oE4xzQg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Dota 2",
@@ -168,7 +180,8 @@ return [
         "genre" => 'MOBA',
         "rating" => 8.1,
         "thumbnail" => 'https://e.snmc.io/lk/l/x/481cf50670f6cd67d8fa5a899c174284/5288890',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Halo: Combat Evolved",
@@ -180,7 +193,8 @@ return [
         "genre" => 'First-person shooter',
         "rating" => 8.1,
         "thumbnail" => 'https://upload.wikimedia.org/wikipedia/en/8/80/Halo_-_Combat_Evolved_%28XBox_version_-_box_art%29.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "God of War",
@@ -192,7 +206,8 @@ return [
         "genre" => 'Action-adventure, hack and slash',
         "rating" => 8.1,
         "thumbnail" => 'https://upload.wikimedia.org/wikipedia/en/a/a7/God_of_War_4_cover.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "The Legend of Zelda: Ocarina of Time",
@@ -204,7 +219,8 @@ return [
         "genre" => 'Action-adventure',
         "rating" => 8.1,
         "thumbnail" => 'https://www.staynerd.com/wp-content/uploads/ocarina_of_time_ft-img.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Tony Hawk's Pro Skater 2",
@@ -216,7 +232,8 @@ return [
         "genre" => 'Sports',
         "rating" => 8.1,
         "thumbnail" => 'https://assets-prd.ignimgs.com/2022/01/29/tony-hawk-pro-skater-2-button-edit-1643485674590.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "BioShock",
@@ -228,7 +245,8 @@ return [
         "genre" => 'First-person shooter',
         "rating" => 8.1,
         "thumbnail" => 'https://upload.wikimedia.org/wikipedia/en/6/6d/BioShock_cover.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
     [
         "title" => "Journey",
@@ -240,6 +258,7 @@ return [
         "genre" => 'Adventure, art game',
         "rating" => 8.1,
         "thumbnail" => 'https://s3.amazonaws.com/prod-media.gameinformer.com/styles/product_box_art/s3/2019/08/07/6cf0190d/journey.jpg',
-        "early_access" => 0
+        "early_access" => 0,
+        "description" => 'lorem ipsum dolor sit am'
     ],
 ];
