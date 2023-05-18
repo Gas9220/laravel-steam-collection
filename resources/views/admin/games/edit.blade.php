8d2207fe894062e917a337ccbd3954acccbbac11
P4anix <pancok_1@yahoo.it>
2023-05-18 19:58:07 +0200

    fix edit

--- resources/views/admin/games/edit.blade.php
+++ resources/views/admin/games/edit.blade.php
@@ -1,71 +1,68 @@
 @extends('layout.app')
 
-<div class="container">
-    <h1>Edita un nuovo videogioco</h1>
-</div>
-
-
 @section('page.main')
-
-<form action="{{route('games.update', $game->id)}}" method="POST">
-    @csrf
-    @method('PUT')
-    <div class="form-group">
-        <label for="title">titolo</label>
-        <input type="text" value="{{old('title',$game->title)}}" class="form-control" id="title" name="title">
-    </div>
-    <div class="form-group">
-        <label for="publisher">publisher</label>
-        <input type="text" value="{{old('publisher',$game->publisher)}}" class="form-control" id="publisher" name="publisher">
-    </div>
-    <div class="form-group">
-        <label for="publication_year">publication_year</label>
-        <input type="date" value="{{old('publication_year',$game->publication_year)}}" class="form-control" id="publication_year" name="publication_year">
-    </div>
-    <div class="form-group">
-        <label for="developers">developers</label>
-        <input type="text" value="{{old('developers',$game->developers)}}" class="form-control" id="developers" name="developers">
-    </div>
-    <div class="form-group">
-        <label for="platforms">platforms</label>
-        <input type="text" value="{{old('platforms',$game->platforms)}}" class="form-control" id="platforms" name="platforms">
-    </div>
-    <div class="form-group">
-        <label for="pegi">pegi</label>
-        <select class="form-select" id="tipo" name="pegi">
-            <option selected>Pegi</option>
-            <option value="3" {{old('pegi', $game->pegi)==3 ? 'selected' : null}}>3</option>
-            <option value="7" {{old('pegi', $game->pegi)==7 ? 'selected' : null}}>7</option>
-            <option value="12" {{old('pegi', $game->pegi)==12 ? 'selected' : null}}>12</option>
-            <option value="16" {{old('pegi', $game->pegi)==16 ? 'selected' : null}}>16</option>
-            <option value="18" {{old('pegi', $game->pegi)==18 ? 'selected' : null}}>18</option>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="genre">genre</label>
-        <input type="text" value="{{old('genre',$game->genre)}}" class="form-control" id="genre" name="genre">
-    </div>
-    <div class="form-group">
-        <label for="rating">rating</label>
-        <input type="number" value="{{old('rating',$game->rating)}}" class="form-control" id="rating" name="rating">
-    </div>
-    <div class="form-group">
-        <label for="thumbnail">thumbnail</label>
-        <textarea class="form-control" id="thumbnail" name="thumbnail" rows="3">{{old('thumbnail',$game->thumbnail)}}"</textarea>
-    </div>
-    <div class="form-group">
-        <label for="description">description</label>
-        <textarea class="form-control" id="description" name="description" rows="3">{{old('description',$game->description)}}"</textarea>
-    </div>
-    <div class="form-group">
-        <label for="early_access">early_access</label>
-        <select class="form-select" id="early_access" name="early_access">
-            <option selected>early_access</option>
-            <option value="0" {{old('early_access', $game->early_access)==0 ? 'selected' : null}}>0</option>
-            <option value="1" {{old('early_access', $game->early_access)==1 ? 'selected' : null}}>1</option>
-        </select>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div class="container-fluid">
+    <h1>Edit: {{$game->title}}</h1>
+    <form action="{{route('games.update', $game->id)}}" method="POST">
+        @csrf
+        @method('PUT')
+        <div class="form-group">
+            <label for="title">Titolo</label>
+            <input type="text" value="{{old('title',$game->title)}}" class="form-control" id="title" name="title">
+        </div>
+        <div class="form-group">
+            <label for="publisher">Publisher</label>
+            <input type="text" value="{{old('publisher',$game->publisher)}}" class="form-control" id="publisher" name="publisher">
+        </div>
+        <div class="form-group">
+            <label for="publication_year">Publication year</label>
+            <input type="date" value="{{old('publication_year',$game->publication_year)}}" class="form-control" id="publication_year" name="publication_year">
+        </div>
+        <div class="form-group">
+            <label for="developers">Developers</label>
+            <input type="text" value="{{old('developers',$game->developers)}}" class="form-control" id="developers" name="developers">
+        </div>
+        <div class="form-group">
+            <label for="platforms">Platforms</label>
+            <input type="text" value="{{old('platforms',$game->platforms)}}" class="form-control" id="platforms" name="platforms">
+        </div>
+        <div class="form-group">
+            <label for="pegi">Pegi</label>
+            <select class="form-select" id="tipo" name="pegi">
+                <option selected>Pegi</option>
+                <option value="3" {{old('pegi', $game->pegi)==3 ? 'selected' : null}}>3</option>
+                <option value="7" {{old('pegi', $game->pegi)==7 ? 'selected' : null}}>7</option>
+                <option value="12" {{old('pegi', $game->pegi)==12 ? 'selected' : null}}>12</option>
+                <option value="16" {{old('pegi', $game->pegi)==16 ? 'selected' : null}}>16</option>
+                <option value="18" {{old('pegi', $game->pegi)==18 ? 'selected' : null}}>18</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="genre">Genre</label>
+            <input type="text" value="{{old('genre',$game->genre)}}" class="form-control" id="genre" name="genre">
+        </div>
+        <div class="form-group">
+            <label for="rating">Rating</label>
+            <input type="number" value="{{old('rating',$game->rating)}}" class="form-control" id="rating" name="rating">
+        </div>
+        <div class="form-group">
+            <label for="thumbnail">Thumbnail</label>
+            <textarea class="form-control" id="thumbnail" name="thumbnail" rows="3">{{old('thumbnail',$game->thumbnail)}}"</textarea>
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea class="form-control" id="description" name="description" rows="3">{{old('description',$game->description)}}"</textarea>
+        </div>
+        <div class="form-group">
+            <label for="early_access">Early access</label>
+            <select class="form-select" id="early_access" name="early_access">
+                <option selected>Select an option</option>
+                <option value="0" {{old('early_access', $game->early_access)==0 ? 'selected' : null}}>0</option>
+                <option value="1" {{old('early_access', $game->early_access)==1 ? 'selected' : null}}>1</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 
 @endsection
--- resources/views/admin/games/index.blade.php
+++ resources/views/admin/games/index.blade.php
@@ -1,40 +1,48 @@
 @extends('layout.app')
 @section('page.main')
-<h1 class="text-center">I tuoi Comics</h1>
-<table class="table table-striped">
+<div class="container-fluid">
+    <div class="d-flex justify-content-between align-items-center">
+        <h1 class="text-center">I tuoi Games</h1>
+        <a href="{{ route('games.create') }}" class="btn btn-primary btn-sm me-2 p-2">Create</a>
+    </div>
+
+    <table class="table table-striped">
         <thead>
             <tr>
-              <th scope="col">Title</th>
-              <th scope="col">Publisher</th>
-              <th scope="col">Developers</th>
-              <th scope="col">Platforms</th>
-              <th scope="col">Pegi</th>
-              <th scope="col">Genre</th>
-              <th scope="col">Rating</th>
-              <th scope="col">Early Access</th>
-              <th scope="col">Description</th>
+                <th scope="col">Title</th>
+                <th scope="col">Publisher</th>
+                <th scope="col">Developers</th>
+                <th scope="col">Platforms</th>
+                <th scope="col">Pegi</th>
+                <th scope="col">Genre</th>
+                <th scope="col">Rating</th>
+                <th scope="col">Early Access</th>
+                <th scope="col">Description</th>
             </tr>
         </thead>
-    <tbody class="table-group-divider">
-        @foreach ($games as $game) 
-        <tr>
-          <th scope="row">{{ $game->title }}</th>
-          <td>{{ $game->publisher }}</td>
-          <td>{{ $game->developers }}</td>
-          <td>{{ $game->platforms }}</td>
-          <td>{{ $game->pegi }}</td>
-          <td>{{ $game->genre }}</td>
-          <td>{{ $game->rating }}</td>
-          <td>{{ $game->early_access }}</td>
-          <td>{{ $game->description }}</td>
-          <td>
-            <ul class="list-unstyled d-flex m-0 gap-1 justify-content-end">
-              <li><a href="{{ route('games.show', $game->id)}}" class="btn btn-primary">Show </a></li>
-              <li><a href="{{ route('games.edit', $game->id)}}" class="btn btn-success">Edit </a></li>
-            </ul>
-          </td>
-        </tr>
-      </tbody>
-      @endforeach
- </table>
+        <tbody class="table-group-divider">
+            @foreach ($games as $game)
+            <tr>
+                <th scope="row">{{ $game->title }}</th>
+                <td>{{ $game->publisher }}</td>
+                <td>{{ $game->developers }}</td>
+                <td>{{ $game->platforms }}</td>
+                <td>{{ $game->pegi }}</td>
+                <td>{{ $game->genre }}</td>
+                <td>{{ $game->rating }}</td>
+                <td>{{ $game->early_access == 0 ? 'no' : 'yes'}}</td>
+                <td>{{ $game->description }}</td>
+                <td>
+                    <ul class="list-unstyled d-flex m-0 gap-1 justify-content-end">
+                        <li><a href="{{ route('games.show', $game->id)}}" class="btn btn-primary">Show </a></li>
+                        <li><a href="{{ route('games.edit', $game->id)}}" class="btn btn-success">Edit </a></li>
+                    </ul>
+                </td>
+            </tr>
+        </tbody>
+        @endforeach
+    </table>
+
+</div>
+
 @endsection
