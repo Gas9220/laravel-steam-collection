145b75f090a6657b8a053383e879095a8ca3dab7
Gaspare <Gas9220@users.noreply.github.com>
2023-05-18 19:21:35 +0200

    Header styling

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,13 @@
     <title>@yield('page.title')</title>
     @vite('resources/js/app.js')
 </head>
+
 <body>
+    @include('partials.header')
+    <main>
+        @yield('page.main')
+    </main>
+    @include('partials.footer')
 </body>
+
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <div class="container-fluid">
+        <div class="d-flex align-items-center p-2">
+            <img src="https://cdn.freebiesupply.com/images/large/2x/steam-logo-transparent.png" alt="logo"
+                class="logo">
+            <h1 class="mb-0 ms-2 header-text">Steam</h1>
+        </div>
+    </div>
+</header>
