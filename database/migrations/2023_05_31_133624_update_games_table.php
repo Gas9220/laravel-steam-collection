0d7691177f586984f0d9f7efa0c13402b312f26d
Gaspare <Gas9220@users.noreply.github.com>
2023-05-31 15:44:29 +0200

    Games table updated

---
+++ database/migrations/2023_05_31_133624_update_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->float('price', 4, 2)->default(0);
+            $table->boolean('is_highlighted')->default(0);
+            $table->integer('discount')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->dropColumn('price');
+            $table->dropColumn('is_highlighted');
+            $table->dropColumn('discount');
+        });
+    }
+};
