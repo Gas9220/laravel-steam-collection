2277cb3a9f8a44c91befde629d40ce8acc0979e2
Gaspare <Gas9220@users.noreply.github.com>
2023-05-31 10:43:51 +0200

    Genre model and game relationships setup

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Game extends Model
+class Genre extends Model
 {
     use HasFactory;
 
-    protected $guarded = [];
-
-    public function genres(){
-        return $this->belongsToMany(Genre::class)->withTimestamps();
+    public function games() {
+        return $this->belongsToMany(Game::class);
     }
 }
